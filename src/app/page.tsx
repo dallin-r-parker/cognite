8052ad1dd444910a73e8d02a69bb2e6b8c187f2c
Dallin <13424017+dallin-r-parker@users.noreply.github.com>
2025-04-09 12:23:30 -0500

    feat: adding handlers and date-fns package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,68 @@
 "use client";
 
+import ChatWindow from "./components/ChatWindow";
+import FriendsList from "./components/FriendsList";
+import Modal from "./components/Modal";
+import { format } from "date-fns";
+import { useFriends } from "./context/FriendsContext";
+import { useState } from "react";
+
+interface Message {
+  text: string;
+  sender: string;
+  time: string;
+}
+
 export default function Home() {
+  const { friends, addFriend, updateFriendLastSeen } = useFriends();
+
+  const [selectedFriend, setSelectedFriend] = useState<string | null>(
+    friends[0].name
+  );
+  const [messages, setMessages] = useState<Record<string, Message[]>>({});
+  const [input, setInput] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSend = () => {
+    if (selectedFriend && input.trim()) {
+      setMessages((prev) => ({
+        ...prev,
+        [selectedFriend]: [
+          ...(prev[selectedFriend] || []),
+          { text: input, sender: "You", time: format(new Date(), "h:mm a") },
+        ],
+      }));
+      updateFriendLastSeen(selectedFriend); // This will move the friend to the top
+      setInput("");
+    }
+  };
+
+  const handleAddFriend = (name: string) => {
+    addFriend(name);
+    setIsOpen(false);
+  };
+
   return (
     <>
-      <h1>Home</h1>
+      <FriendsList
+        friends={friends}
+        selectedFriend={selectedFriend}
+        onSelectFriend={setSelectedFriend}
+        onAddFriend={() => setIsOpen(true)}
+      />
+      <ChatWindow
+        selectedFriend={selectedFriend}
+        messages={messages}
+        input={input}
+        onInputChange={setInput}
+        onSend={handleSend}
+      />
+      <Modal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        handleAddFriend={handleAddFriend}
+        handleInput={setInput}
+      />
     </>
   );
 }
